ba0aa4ec807f0ddf1b078f85d2319588f4de77bb
Agayev Pervin Nadir <Agaye_jz58@itstep.edu.az>
2023-11-07 12:50:08 +0400

    incomplitet

---
+++ practic/ViewModels/ViewModelPages/ViewModeEdit.cs
@@ -0,0 +1,54 @@
+﻿using practic.Commands;
+using practic.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace practic.ViewModels.ViewModelPages
+{
+    public class ViewModeEdit:INotifyPropertyChanged
+    {
+        private User? user1;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public User? user { get => user1; set { user1 = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(user))); } }
+        public ICommand? EditCommand { get; set; }
+        public ObservableCollection<User> Users { get; }
+        public int Index { get; }
+
+        public ViewModeEdit(ObservableCollection<User>users,int index)
+        {
+            EditCommand = new Command(Editcomand, CanEditCommand);
+            Users = users;
+            Index = index;
+            user= users[index]; 
+        }
+        public void Editcomand(object? param)
+        {
+            Users[Index] = new();
+            Users[Index]=user!;
+            File.WriteAllText("C:\\Users\\Agaye_jz58\\source\\repos\\practic\\practic\\Database\\Users.json", JsonSerializer.Serialize(Users, new JsonSerializerOptions() { WriteIndented = true }));
+
+        }
+        public bool CanEditCommand(object? param)
+        {
+            return !string.IsNullOrWhiteSpace(user.name) &&
+                      !string.IsNullOrWhiteSpace(user.username) &&
+                      !string.IsNullOrWhiteSpace(user.email) &&
+                      !string.IsNullOrWhiteSpace(user.id.ToString()) &&
+                      !string.IsNullOrWhiteSpace(user.address.city) &&
+                      !string.IsNullOrWhiteSpace(user.address.street) &&
+                      !string.IsNullOrWhiteSpace(user.company.name) &&
+                      !string.IsNullOrWhiteSpace(user.company.bs);
+        }
+
+    }
+}
---
+++ practic/ViewModels/ViewModelPages/ViewModelEntry.cs
@@ -0,0 +1,90 @@
+﻿using practic.Commands;
+using practic.Models;
+using practic.Views.ViewPages;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace practic.ViewModels.ViewModelPages
+{
+    public class ViewModelEntry:INotifyPropertyChanged
+    {
+        private User? user1;
+
+        public ObservableCollection<User>?users { get; set; }
+        public User? user { get => user1; set { user1 = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(user))); } }
+
+        public ICommand ?AddCommand { get; set; }
+        public ICommand ? RemoveCommand { get; set; }
+        public ICommand ? EditCommand { get; set; }
+        public ViewModelEntry()
+        {
+            user = new();
+            users = new(JsonSerializer.Deserialize<List<User>>(File.ReadAllText("C:\\Users\\Agaye_jz58\\source\\repos\\practic\\practic\\Database\\Users.json"))!);
+
+            RemoveCommand = new Command(Removecomand, CanremoveCommand);
+            AddCommand = new Command(AddComand,CanAddCommand);
+            EditCommand = new Command(Editcomand, CanEditCommand);
+           
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void AddComand(object? param)
+        {
+            Page? page = ((Page)param);
+            //var stackpanel = page.FindName("stackpanell");
+            //var combo = ((StackPanel)stackpanel).FindName("combobox");
+            //user = users[((ComboBox)combo).SelectedIndex];
+            Page pagee = new ViewAddPage();
+            var datacon = new ViewModelAddPage(users);
+            pagee.DataContext = datacon;
+            page.NavigationService.Navigate(pagee);
+                
+        }
+        public bool CanAddCommand(object? param)
+        {
+            return true;
+        }
+        public void Removecomand(object? param)
+        {
+            users.RemoveAt((int)param);
+            File.WriteAllText("C:\\Users\\Agaye_jz58\\source\\repos\\practic\\practic\\Database\\Users.json", JsonSerializer.Serialize(users, new JsonSerializerOptions() { WriteIndented = true }));
+
+
+        }
+        public bool CanremoveCommand(object? param)
+        {
+            return (int)param!=-1;
+        }
+        public void Editcomand(object? param)
+        {
+            Page? page = ((Page)param);
+            var stackpanel = page.FindName("stackpanell");
+            var combo = ((StackPanel)stackpanel).FindName("combobox");
+            user = users[((ComboBox)combo).SelectedIndex];
+            Page pagee = new ViewEdit();
+            var datacon = new ViewModeEdit(users, ((ComboBox)combo).SelectedIndex);
+            pagee.DataContext = datacon;
+            page.NavigationService.Navigate(pagee);
+
+
+        }
+        public bool CanEditCommand(object? param)
+        {
+            Page? page = ((Page)param);
+            var stackpanel = page.FindName("stackpanell");
+            var combo = ((StackPanel)stackpanel).FindName("combobox");
+            return ((ComboBox)combo).SelectedIndex != -1;
+
+        }
+    }
+}
